68ff1330eb12acc85d09580f06dcd9f372b8871a
tuongnm <manhtuong.dev@gmail.com>
2024-01-24 10:54:59 +0700

    Update url

--- Vocabulary/urls.py
+++ Vocabulary/urls.py
@@ -16,24 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from app_api_gateway.views import index
-from app_authentication.views import CustomTokenView
-from app_translate.views import Translate
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api/', include('app_api_gateway.urls')),
-    path('', index),
-]
-
-urlpatterns += [
-    path('o/token/', CustomTokenView.as_view(), name='token'),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('translate/', Translate.as_view(), name='translate'),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- app_api_gateway/urls.py
+++ app_api_gateway/urls.py
@@ -1,6 +1,9 @@
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework.routers import DefaultRouter
 
-from app_authentication.views import RegisterView
+from app_authentication.views import RegisterView, CustomTokenView
+from app_translate.views import Translate
 from app_vocabulary.views import EnglishVocabularyViewSet
 
 router = DefaultRouter()
@@ -8,4 +11,14 @@ router = DefaultRouter()
 router.register('vocabulary', EnglishVocabularyViewSet, basename='vocabulary')
 router.register('register', RegisterView, basename='register')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('o/token/', CustomTokenView.as_view(), name='token'),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('translate/', Translate.as_view(), name='translate'),
+]
+
+urlpatterns += router.urls
