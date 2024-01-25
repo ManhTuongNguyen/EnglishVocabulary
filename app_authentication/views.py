e2a9209b2c3aac7087a378b8f2bc2af9d7ab21aa
tuongnm <manhtuong.dev@gmail.com>
2024-01-25 10:31:30 +0700

    Update swagger

--- app_authentication/views.py
+++ app_authentication/views.py
@@ -1,18 +1,48 @@
 from django.contrib.auth.models import User
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, inline_serializer, OpenApiResponse
 from oauth2_provider.views import TokenView
-from rest_framework import generics
+from rest_framework import generics, serializers
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 
 from Vocabulary.settings import O2AuthClient
 from app_authentication.serializers import RegisterSerializer
 
 
-class CustomTokenView(TokenView):
+@extend_schema(tags=['Token'], operation_id='token')
+class CustomTokenView(TokenView, APIView):
     @method_decorator(sensitive_post_parameters("password"))
+    @extend_schema(
+        request=inline_serializer(
+            name="InlineTokenSerializer",
+            fields={
+                "username": serializers.CharField(required=False),
+                "password": serializers.CharField(required=False),
+                "refresh_token": serializers.CharField(required=False),
+                "grant_type": serializers.CharField(required=False, default="password",
+                                                    help_text="Example: password, refresh_token"),
+                "client_id": serializers.CharField(required=False, help_text='Nullable'),
+                "client_secret": serializers.CharField(required=False, help_text='Nullable'),
+            },
+        ),
+        responses={
+            200: inline_serializer(
+                name='TokenSerializer',
+                fields={
+                    'access_token': serializers.CharField(),
+                    'expires_in': serializers.IntegerField(),
+                    'token_type': serializers.CharField(),
+                    'scope': serializers.CharField(),
+                    'refresh_token': serializers.CharField(),
+                }
+            ),
+            400: OpenApiResponse(),
+        },
+        description='Get token using username and password or refresh_token',
+    )
     def post(self, request, *args, **kwargs):
         data = request.POST.copy()
         if not data.get('client_id') or not data.get('client_secret'):
